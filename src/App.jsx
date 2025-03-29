d70fc0781a42e77f6a595a36ceabc0b6b631971e
paulsiskind <paulsiskind@gmail.com>
2025-03-29 05:08:16 -0600

    Moar Commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 
 import { useState } from 'react';
 import './App.css'
+import { Navbar } from "./components/sections/Navbar.jsx";
 import { LoadingScreen } from './components/LoadingScreen';
-import { Navbar } from './components/Navbar';
 import "./index.css"
 
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOepn, setMenuOpen] = useState(false)
+
   return ( 
   <>
    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
     <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}
     > 
-      <Navbar />
+      <Navbar menuOepn={menuOepn} setMenuOpen={setMenuOpen}/>
+      <Navbar menuOepn={menuOepn} setMenuOpen={setMenuOpen}/>
     </div>
     </>
   );
--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,6 +1,30 @@
-export const Navbar = () => {
+import { useEffect } from "react"
 
-    return <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,.8)] backdrop-blur-lg border-b border-white/10 shadow-lg"> 
-    {""}
+export const Navbar = (menuOepn, setMenuOpen) => {
+
+    useEffect(() => {
+        document.body.style.overflow = menuOepn ? "hidden" : "";
+    }
+    , [menuOepn]);
+    return (
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,.8)] backdrop-blur-lg border-b border-white/10 shadow-lg"> 
+        <div className="max-w-5xl mx-auto px-4">
+            <div className='flex justify-between items-center h-16'>
+                <a href="#Home" className="text-xl font-mono font-bold text-white">
+                    {" "}
+                    HollyHock<span className="text-blue-500">Automations</span> {" "}
+                </a>
+                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>)}>
+                    &#9776;
+                </div>
+                <div className="hidden md:flex items-center space-x-8">
+                    <a href="#Home" className="text-gray-300 hover:text-white transition-colors">Home</a>
+                    <a href="#About" className="text-gray-300 hover:text-white transition-colors">About</a>
+                    <a href="#Projects" className="text-gray-300 hover:text-white transition-colors">Projects</a>
+                    <a href="#Contact" className="text-gray-300 hover:text-white transition-colors">Contact</a>
+                </div>
+            </div>
+        </div>
     </nav>
+    )
 }
