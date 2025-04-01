1ede2913aee4948802eb9cdfbd5fa733d4770a00
paulsiskind <paulsiskind@gmail.com>
2025-04-01 10:19:08 -0600

    Changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  page: "./",
-})
+  base: './',              // ensure relative paths, e.g. in index.html
+build: {
+    outDir: 'dist',      // gets deployed to github pages
+    assetsDir: 'assets', // static assets (images, js, css, etc.)
+   },
+});
